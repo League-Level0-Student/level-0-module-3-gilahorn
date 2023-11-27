4da1b1bb5962f2cfa5fc1489e1826cb0443eaa6e
gilahorn <gilahorn21@gmail.com>
2023-11-27 08:21:54 -0800

    robot color chooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,6 +2,9 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
@@ -14,10 +17,31 @@ public class RobotColorChooser {
 		//5. Use an if/else statement to set the pen color that the user requested
         //6. If the user doesn't enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
+		Robot rob = new Robot();
+		rob.penDown();
+		rob.setSpeed(100);
+		rob.setPenWidth(10);
 		
-
-		
-
+		String color = JOptionPane.showInputDialog("what color");
+		if(color.equalsIgnoreCase("red")) {
+			rob.setPenColor (Color.red);
+	
+		}
+		else if(color.equalsIgnoreCase("green")) {
+			rob.setPenColor (Color.green);
+		}
+		else if(color.equalsIgnoreCase("blue")) {
+			rob.setPenColor (Color.blue);
+	}
+	else {
+		rob.setRandomPenColor();
+	}
+		for (int i = 0; i<4; i++) {
+			rob.move(100);
+			rob.turn(90);
+		}
+color = JOptionPane.showInputDialog("what color?");
+		rob.hide();
 
 
 	}
